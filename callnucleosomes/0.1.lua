8bb8d2da305b73b738d0447f3c96e0e1b657a987
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-05-21 11:51:18 -0400

    Splits CallNucleosomes module from robtools.

---
+++ callnucleosomes/0.1.lua
@@ -0,0 +1,19 @@
+help([[
+For detailed instructions, go to:
+    https://github.com/srinivasramachandran/CallNucleosomes
+
+This module loads the following modules and their requirements:
+    - perl/5.22.4
+]])
+
+whatis("Version: 0.1")
+whatis("Keywords: NGS, Nucleosomes Calling")
+whatis("URL: https://github.com/srinivasramachandran/CallNucleosomes")
+whatis("Description: Calls nucleosomes position")
+
+always_load("StdEnv/2018.3")
+always_load("perl/5.22.4")
+
+local home = os.getenv("HOME") or ""
+local call_nucleosomes = pathJoin(home, "projects/def-robertf/apps/CallNucleosomes/0.1")
+prepend_path("PERL5LIB", pathJoin(call_nucleosomes, "perl_library"))
--- robtools/2.0.lua
+++ robtools/2.0.lua
@@ -11,7 +11,6 @@ This module loads the following modules and their requirements:
     - robtools/core/2.0
     - python/3.7.4
     - java/1.8.0_121
-    - perl/5.22.4
     - fastqc/0.11.8
     - trimmomatic/0.36
     - bwa/0.7.17
@@ -32,7 +31,6 @@ always_load("StdEnv/2018.3")
 always_load("gcc/7.3.0")
 always_load("python/3.7.4")
 always_load("java/1.8.0_121")
-always_load("perl/5.22.4")
 always_load("fastqc/0.11.8")
 always_load("trimmomatic/0.36")
 always_load("bwa/0.7.17")
@@ -46,8 +44,6 @@ always_load("robtools/core/2.0")
 
 local home = os.getenv("HOME") or ""
 local project = pathJoin(home, "projects/def-robertf")
-local call_nucleosomes = pathJoin(project, "apps/CallNucleosomes/0.1")
-prepend_path("PERL5LIB", pathJoin(call_nucleosomes, "perl_library"))
 local siqchip = pathJoin(project, "app/siQ-ChIP/1.0.0")
 setenv("SIQ_CHIP_BASE", siqchip)
 local trimmomatic_home = os.getenv("EBROOTTRIMMOMATIC") or ""

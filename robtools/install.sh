86e3e15f5bdc428e319481a53b660914d058d536
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-05-25 15:54:07 -0400

    Adds installation script for robtools.

---
+++ robtools/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$ROBTOOLS" ]
+then
+  echo "ROBTOOLS environment variable must be defined, please load a robtools module"
+  exit 1
+fi
+
+
+VENV="$ROBTOOLS"/venv
+
+if [ "$1" == "clean" ]
+then
+  echo "Removing python virtual environment at $VENV"
+  rm -R "$VENV"
+  exit 0
+fi
+
+if [ ! -d "$ROBTOOLS" ]
+then
+  echo "Creating folder $ROBTOOLS"
+  mkdir -p $ROBTOOLS
+fi
+cd "$ROBTOOLS"
+if [ ! -d "$ROBTOOLS/.git" ]
+then
+  echo "Cloning robtools in folder $ROBTOOLS and checking out version $ROBTOOLS_VERSION"
+  git clone https://github.com/francoisrobertlab/robtools.git .
+  if [ "$ROBTOOLS_VERSION" == "2.0" ]
+  then
+    git checkout master
+  elif [ "$ROBTOOLS_VERSION" == "1.2" ]
+  then
+    git checkout hotfix-1.2
+  else
+    git checkout "$ROBTOOLS_VERSION"
+  fi
+fi
+if [ ! -d "$VENV" ]
+then
+  echo "Creating python virtual environment at $VENV"
+  python3 -m venv "$VENV"
+fi
+VERSION=$(git --git-dir="$ROBTOOLS"/.git rev-parse --abbrev-ref HEAD)
+echo "Updating python libraries using $VERSION"
+"$VENV"/bin/pip uninstall -y RobTools
+"$VENV"/bin/pip install git+file://"$ROBTOOLS"@"$VERSION"
+find "$VENV" -type f -perm 750 -exec sed -i "1 s|^.*$|#!/usr/bin/env python3|g" {} \;

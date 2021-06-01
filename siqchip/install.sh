a02cd6eb9fe9264f8872c252586f2ae841c54a6e
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-01 12:04:51 -0400

    Adds installation script for siqchip.

---
+++ siqchip/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$SIQCHIP" ]
+then
+  echo "SIQCHIP environment variable must be defined, please load a siqchip module"
+  exit 1
+fi
+
+
+# Clone siqchip, if not already cloned.
+if [ ! -d "$SIQCHIP" ]
+then
+  echo "Creating folder $SIQCHIP"
+  mkdir -p "$SIQCHIP"
+fi
+cd "$SIQCHIP" || { echo "Folder $SIQCHIP does not exists"; exit 1; }
+if [ ! -d "$SIQCHIP/.git" ]
+then
+  echo "Cloning siqchip in folder $SIQCHIP"
+  git clone https://github.com/BradleyDickson/siQ-ChIP.git .
+fi
+echo "Checking out version $SIQCHIP_VERSION"
+git checkout "v$SIQCHIP_VERSION"

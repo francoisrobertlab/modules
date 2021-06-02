c55ea596b76c307aa1b49fa840d0a29ddeaa0ef7
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-01 20:06:05 -0400

    Use optimized bioconductor library for chipexoqual module.

--- chipexoqual/1.12.0.lua
+++ chipexoqual/1.12.0.lua
@@ -7,10 +7,10 @@ This module sets the following environment variables:
     R_PROFILE_USER:    allows to configure CPAN automatically
 
 This module loads the following modules:
-    - r/4.0.2
+    - r-bundle-bioconductor/3.12
 
 You will need to load all module(s) on any one of the lines below before the "chipexoqual" module is available to load.
-    gcc/7.3.0
+    gcc/9.3.0
 ]])
 
 whatis("Version: 1.0 or later, but before 1,1")
@@ -18,8 +18,8 @@ whatis("Keywords: ChIPexoQual, Utility")
 whatis("URL: https://www.bioconductor.org/packages/release/bioc/html/ChIPexoQual.html")
 whatis("Description: Quality control pipeline for ChIP-exo/nexus data.")
 
-prereq("gcc/7.3.0")
-always_load("r/4.0.2")
+prereq("gcc/9.3.0")
+always_load("r-bundle-bioconductor/3.12")
 
 local module_path = myFileName()
 local module_name = myModuleFullName()

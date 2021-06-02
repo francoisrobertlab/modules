a581baa86cfb305a51ccfe537bd05f29fb43b948
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-01 21:06:42 -0400

    Fixes comment in sipchip module.

--- siqchip/1.0.0.lua
+++ siqchip/1.0.0.lua
@@ -18,7 +18,7 @@ local module_name = myModuleFullName()
 local module_base = module_path:sub(1,module_path:find(module_name,1,true)-2)
 local apps_base = module_base:gsub("(.*)/(.*)","%1")
 local home = pathJoin(apps_base, module_name)
-setenv("SIQ_CHIP_BASE", home)   # For compatibility with robtools code
+setenv("SIQ_CHIP_BASE", home)   -- For compatibility with robtools code
 setenv("SIQCHIP", home)
 setenv("SIQCHIP_VERSION", module_version)
 setenv("SIQCHIP_MODULE", pathJoin(module_base, myModuleName()))

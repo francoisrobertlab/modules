144d4b67b54c78cf688cf2a4408b5bbaffe1dc23
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-03 13:49:17 -0400

    Adds installation script for GEM.

---
+++ gem/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$GEM" ]
+then
+  echo "GEM environment variable must be defined, please load a gem module"
+  exit 1
+fi
+
+
+# Download GEM.
+if [ ! -d "$GEM" ]
+then
+  echo "Creating folder $GEM"
+  mkdir -p "$GEM"
+fi
+cd "$GEM" || { echo "Folder $GEM does not exists"; exit 1; }
+echo "Downloading GEM version $GEM_VERSION"
+GEM_TAR=gem.v"$GEM_VERSION".tar.gz
+wget https://groups.csail.mit.edu/cgs/gem/download/"$GEM_TAR"
+tar -xf "$GEM_TAR"

f921b8bbafdeb111f94e1426f52f9e4c9c245480
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-07 11:10:23 -0400

    Removes useless pip uninstall command in robtools install.sh.

--- robtools/install.sh
+++ robtools/install.sh
@@ -30,6 +30,5 @@ echo "Creating python virtual environment at $VENV"
 python3 -m venv "$VENV"
 VERSION=$(git --git-dir="$ROBTOOLS"/.git rev-parse --abbrev-ref HEAD)
 echo "Updating python libraries using $VERSION"
-"$VENV"/bin/pip uninstall -y RobTools
 "$VENV"/bin/pip install git+file://"$ROBTOOLS"@"$VERSION"
 find "$VENV" -type f -perm 750 -exec sed -i "1 s|^.*$|#!/usr/bin/env python3|g" {} \;
